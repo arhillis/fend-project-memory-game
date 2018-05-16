2d112e30ee0c8acb3bf913aa695828a417172533
Andrielle Hillis <andriellerhillis@gmail.com>
2018-05-15 22:19:58 -0500

    Added the timer feature

--- js/app.js
+++ js/app.js
@@ -14,54 +14,89 @@
 // Shuffle function from http://stackoverflow.com/a/2450976
 
 
-function newGame(){
+function newGame() {
 
     layOutCards();
 
-    let cardEls = document.querySelectorAll(".card");    
+    let cardEls = document.querySelectorAll(".card");
     const movesEl = document.querySelector(".moves");
+    const secEl = document.querySelector("#secs");
+    const minEl = document.querySelector("#mins");
 
     let faceUpCards = [];
 
-    let moves = 0;
+    let moves = 0,
+        playing = false;
 
+    let timer;
 
     for (card of cardEls) {
         card.onclick = function () {
-            if(!this.classList.contains("face-up")){
+
+            if (!playing) {
+                playing = true;
+
+                timer = window.setInterval(function () {
+                    clockTick();
+                }, 1000)
+
+            }
+
+            if (!this.classList.contains("face-up")) {
                 this.classList.add("face-up");
                 faceUpCards.push(this);
 
-                if(faceUpCards.length === 2){
+                if (faceUpCards.length === 2) {
 
-                    window.setTimeout(function(){
+                    window.setTimeout(function () {
                         endTurn(faceUpCards[0], faceUpCards[1]);
                     }, 1000);
                 }
 
-                function endTurn(card1, card2){
+                function endTurn(card1, card2) {
                     let match = checkMatch(card1, card2);
 
-                    if(match){
+                    if (match) {
                         card1.classList.add("matched");
                         card2.classList.add("matched");
-                    }else{
+                    } else {
                         card1.classList.remove("face-up");
                         card2.classList.remove("face-up");
                     }
 
                     moves++;
-                    movesEl.textContent = moves.toString();                    
+                    movesEl.textContent = moves.toString();
                     faceUpCards = [];
 
-                    
+                    if (document.querySelectorAll(".matched").length == 16) {
+                        resetGame();
+                    }
+
                 }
             }
-            
+
         }
-    }  
-    
-    function layOutCards(){
+
+        function clockTick() {
+
+            let secs = parseInt(secEl.textContent);
+
+            if (secs === 59) {
+                secEl.textContent = "00";
+                let mins = parseInt(minEl.textContent);
+                increment(mins, minEl);
+            } else {
+                increment(secs, secEl);
+            }
+
+            function increment(num, element) {
+                num++;
+                element.textContent = (num < 10) ? "0" + num.toString() : num.toString();
+            }
+        }
+    }
+
+    function layOutCards() {
         const deck = document.querySelector(".deck");
         let cards = [
             "fa-diamond",
@@ -81,9 +116,9 @@ function newGame(){
             "fa-bicycle",
             "fa-bomb"
         ];
-    
+
         shuffle(cards);
-        
+
         cardHTML = ``;
         for (let card of cards) {
             cardHTML += ` <li class="card">
@@ -97,7 +132,7 @@ function newGame(){
     function shuffle(array) {
         var currentIndex = array.length,
             temporaryValue, randomIndex;
-    
+
         while (currentIndex !== 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex -= 1;
@@ -105,12 +140,26 @@ function newGame(){
             array[currentIndex] = array[randomIndex];
             array[randomIndex] = temporaryValue;
         }
-    
-        return array;
-    }  
 
-    function checkMatch(firstCard, secondCard){
+        return array;
+    }
+
+    function checkMatch(firstCard, secondCard) {
         return firstCard.children[0].classList[1] === secondCard.children[0].classList[1];
+    }
+
+    function resetGame() {
+        alert("matched!");
+        playing = false;
+        window.clearInterval(timer);
+        minEl.textContent = "0";
+        secEl.textContent = "00";
+        movesEl.textContent = "0";
+        moves = 0;
+        cardEls.forEach(function (card) {
+            card.classList.remove("matched");
+            card.classList.remove("face-up");
+        })
     }
 }
 
@@ -131,7 +180,7 @@ window.onload = function () {
 
     document.querySelector(".restart").onclick = function () {
         newGame();
-    };  
+    };
 
-    
+
 };
